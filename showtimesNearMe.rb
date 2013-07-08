0fcbb8622005fcd97068c79459a8e9a4fa0e0a16
DrJonOsterman <egarcia2388@gmail.com>
2013-07-08 11:26:35 -0400

    refactor

--- showtimesNearMe.rb
+++ showtimesNearMe.rb
@@ -5,27 +5,25 @@ doc = Nokogiri::HTML(open("movDump.htm"))
 output = File.open("output.txt", "w")
 
 doc.css("#movie_results .movie_results .theater").each do |theater|
-
 	output.write("-------------------\n")
 	output.write(theater.css(".desc .name a").text() + "\n")
-
-	theater.css(".showtimes .show_left .movie").each do |leftMov|
-		output.write(leftMov.css(".name").text() + ": ")
-
-		leftMov.css(".times span").each do |showTime|
-			# add non-fandango movie logic here
-			output.write(showTime.css("a").text() + " ")
+	
+	def printTimes(location, side, out)
+		location.css(".showtimes .show_" + side + " .movie").each do |mov|
+			out.write(mov.css(".name").text() + ": ")
+			mov.css(".times span").each do |showTime|
+				times = showTime.text().scan(/(\d{1,2}:\d\d[[ap]m]?)/)
+				times.each do |time1|
+					time1.each do |time2|
+						out.write(time2 + " ")
+					end
+				end
+			end
+			out.write("\n")
 		end
-		output.write("\n")
 	end
-
-	# theater.css(".showtimes .show_right .movie").each do |rightMov|
-	#  	output.write(rightMov.css(".name").text() + ": ")
-
-	#  	rightMov.css(".times span").each do |showTime|
-	# 		output.write(showTime.css("a").text() + " ")
-	#  	end
-	# 	output.write("\n")
-	# end
+	
+	printTimes(theater, 'left', output)
+	printTimes(theater, 'right', output)
 end
 output.close
